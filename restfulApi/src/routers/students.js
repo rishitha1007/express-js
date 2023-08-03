2525531ae43adfe687fc4ea70f7de573e07587de
atishshingote <atishshingote@gmail.com>
2023-08-03 16:39:36 +0530

    new commit

---
+++ restfulApi/src/routers/students.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = new express.Router();
+const Student = require("../models/student")
+
+router.post("/students", async(req,res) =>{
+    try{
+        const user = new Student(req.body);
+        
+        const createuser = await user.save();
+        res.status(201).send(createuser);
+
+    }catch(e){res.status(400).send(e);}
+})
+
+router.get("/students", async(req,res) =>{    
+    try{        
+        const studentsData = await Student.find();
+        console.log(studentsData);
+        res.send(studentsData);
+    }catch(e){
+        res.send(e);
+    }
+})
+
+router.get("/students/:id", async(req,res) =>{    
+    try{ 
+        const _id =req.params.id;       
+        const studentData = await Student.findById(_id);
+        console.log(studentData);
+        res.send(studentData);
+    }catch(e){
+        res.send(e);
+    }
+})
+
+router.delete("/students/:id", async(req,res) =>{    
+    try{        
+        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+        }
+        res.send(deleteStudent);
+    }catch(e){
+        res.status(500).send(e);
+    }
+})
+// Update the student by its ID
+router.patch("/students/:id", async(req,res) =>{    
+    try{  
+        const _id =req.params.id;      
+        const updateStudents = await Student.findByIdAndUpdate(_id , req.body , {
+            new : true
+        });
+        res.send(updateStudents);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+module.exports = router;
